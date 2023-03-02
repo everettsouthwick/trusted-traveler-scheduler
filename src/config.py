f5a58e1042446df29d77d02dd9c7ada8214f3dcc
Everett Southwick <everett@everettsouthwick.com>
2023-03-02 11:04:11 -0500

    add src

---
+++ src/config.py
@@ -0,0 +1,74 @@
+import json
+import os
+import sys
+from typing import Any, Dict
+
+from .general import NotificationLevel
+
+CONFIG_FILE_NAME = "config.json"
+
+
+class Config:
+    def __init__(self):
+        # Default values are set
+        self.location_ids = []
+        self.notification_urls = []
+        self.notification_level = NotificationLevel.INFO
+        self.retrieval_interval = 24
+       
+        # Read the config file
+        config = self._get_config()
+
+        # Set the configuration values if provided
+        try:
+            self._parse_config(config)
+        except TypeError as err:
+            print("Error in configuration file:")
+            print(err)
+            sys.exit()
+
+    def _get_config(self) -> Dict[str, Any]:
+        project_dir = os.path.dirname(os.path.dirname(__file__))
+        config_file = project_dir + "/" + CONFIG_FILE_NAME
+
+        config = {}
+        try:
+            with open(config_file) as file:
+                config = json.load(file)
+        except FileNotFoundError:
+            pass
+
+        return config
+
+    # This method ensures the configuration values are correct and the right types.
+    # Defaults are already set in the constructor to ensure a value is never null.
+    def _parse_config(self, config: Dict[str, Any]) -> None:
+        if "location_ids" in config:
+            self.location_ids = config["location_ids"]
+
+            if not isinstance(self.location_ids, (list, int)):
+                raise TypeError("'location_ids' must be a list or integer")
+
+        if "notification_urls" in config:
+            self.notification_urls = config["notification_urls"]
+
+            if not isinstance(self.notification_urls, (list, str)):
+                raise TypeError("'notification_urls' must be a list or string")
+
+        if "notification_level" in config:
+            self.notification_level = config["notification_level"]
+
+            if not isinstance(self.notification_level, int):
+                raise TypeError("'notification_level' must be an integer")
+
+        if "retrieval_interval" in config:
+            self.retrieval_interval = config["retrieval_interval"]
+
+            if not isinstance(self.retrieval_interval, int):
+                raise TypeError("'retrieval_interval' must be an integer")
+
+            if self.retrieval_interval < 1:
+                print(
+                    f"Setting 'retrieval_interval' to one as {self.retrieval_interval} hours is too low"
+                )
+                self.retrieval_interval = 1
