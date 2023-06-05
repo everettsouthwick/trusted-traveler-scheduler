8c8aab80714259dea80a42076751d816775335e3
Everett Southwick <everett@southwick.dev>
2023-06-04 21:10:26 -0400

    chore(documentation): add docs to all methods

--- src/notification_level.py
+++ src/notification_level.py
@@ -1,5 +1,12 @@
 from enum import IntEnum
 
 class NotificationLevel(IntEnum):
+    """
+    An enumeration representing the different levels of notification that can be used in the application.
+
+    The available notification levels are:
+    - INFO: Used for general information messages.
+    - ERROR: Used for error messages.
+    """
     INFO = 1
     ERROR = 2
